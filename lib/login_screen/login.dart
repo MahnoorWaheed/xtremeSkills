c4e03ba677b349f650bb76c751ffd10142797432
MahnoorWaheed <mahnoorwaheed8395@gmail.com>
2021-12-07 16:09:32 +0500

    updateeeeeeeeeeee

--- lib/login_screen/login.dart
+++ lib/login_screen/login.dart
@@ -21,7 +21,7 @@ Future<List<PostModel>> getPostApi() async{
   final response =await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
   var data= jsonDecode(response.body.toString());
   if(response.statusCode==200){
-   
+   postlist.clear();
   for(Map i in data){
     postlist.add(PostModel.fromJson(i));
   }
@@ -35,67 +35,84 @@ return postlist;
   Widget build(BuildContext context) {
     return Scaffold(
        backgroundColor: Color(0xffFFFFFF),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            
-          //  Image.asset("assets/images/logo.png", 
-          //  fit: BoxFit.cover,
-          //  width: MediaQuery.of(context).size.width*0.5,
-          //  height: MediaQuery.of(context).size.height*0.3,
-           
-          //  ),
-           
-          //  MyTextField(
-          //    label: "Enter your Email",
-          //  ),
-          //   MyTextField(
-          //     label: "Enter your Password",
-          //   ),
-          //   ActionButton(
-          //     text: "Login",
-          //     ontap: () {
-                
-          //     },
-          //   ),
-          //   Row(
-          //     mainAxisAlignment: MainAxisAlignment.end,
-          //     children: [
-          //     Padding(
-          //       padding: const EdgeInsets.symmetric(horizontal: 7),
-          //       child: MyGestures(
-          //         text: "Forget Password?",
-          //         ontap: (){
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPostApi(),
+              builder: (context, snapshot){
+                if(!snapshot.hasData)
+                {
+                  return Text("reloading");
+                }else
+                {
+                  return ListView.builder(
+                    itemCount: postlist.length,
+                    itemBuilder: (ctx,index){
+                    return   Column(
+                      children: [
+                        Image.asset("assets/images/logo.png", 
+         fit: BoxFit.cover,
+         width: MediaQuery.of(context).size.width*0.5,
+         height: MediaQuery.of(context).size.height*0.3,
+         
+         ),     
+         MyTextField(
+           label: "Enter your Email",
+         ),
+          MyTextField(
+            label: "Enter your Password",
+          ),
+          ActionButton(
+            text: "Login",
+            ontap: () {
+              
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 7),
+              child: MyGestures(
+                text: "Forget Password?",
+                ontap: (){
 
-          //         },
-          //       ),
-          //     ),
-          //   ],),
-          //   Divider(
-          //           color: Colors.black,
-          //           height: 10,
-          //           thickness: 0.2,
-          //           indent: 20,
-          //           endIndent: 20,
-          //         ),
-          // //  
-          //     Row(
-          //       mainAxisAlignment: MainAxisAlignment.center,
-          //       children: [
-          //         Container(
-          //           child: Text("Don't have an Account? "),
-          //         ),
-          //       MyGestures(
-          //         text: "Sign Up Here",
-          //       ontap: (){
+                },
+              ),
+            ),
+          ],),
+          Divider(
+                  color: Colors.black,
+                  height: 10,
+                  thickness: 0.2,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+        //  
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Text("Don't have an Account? "),
+                ),
+              MyGestures(
+                text: "Sign Up Here",
+              ontap: (){
 
-          //       },
-          //       )
-          //     ],),
-          
-        ],
-        ),
+              },
+              ),
+
+            ],),]);
+                  });
+                }
+              }
+              ),
+          )
+        
+        
+      ],
       ),
     );
   }
